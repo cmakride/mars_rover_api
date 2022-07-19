ef166e9da518d5b24e0a025027a6709a7e87c8c8
ChrisMak <cmakride@gmail.com>
2022-07-19 09:24:00 -0400

    Unit Test for API photos response

--- src/main/java/com/chrismakrides/marsroverapi/web/HomeController.java
+++ src/main/java/com/chrismakrides/marsroverapi/web/HomeController.java
@@ -1,4 +1,4 @@
-package com.chrismakrides.marsroverapi;
+package com.chrismakrides.marsroverapi.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
--- src/test/java/com/chrismakrides/marsroverapi/MarsRoverApiTest.java
+++ src/test/java/com/chrismakrides/marsroverapi/MarsRoverApiTest.java
@@ -4,14 +4,16 @@ import org.junit.jupiter.api.Test;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
+import com.chrismakrides.marsroverapi.response.MarsRoverApiResponse;
+
 public class MarsRoverApiTest {
 
   @Test
   public void smallTest() {
     RestTemplate apiCall = new RestTemplate();
-    ResponseEntity<String> response = apiCall.getForEntity(
-        "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=ZnyAXCjmbr4ecRqongfksQqSWtKaVPd6divx0MEa",
-        String.class);
+    ResponseEntity<MarsRoverApiResponse> response = apiCall.getForEntity(
+        "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=2&api_key=ZnyAXCjmbr4ecRqongfksQqSWtKaVPd6divx0MEa",
+        MarsRoverApiResponse.class);
     System.out.println(response.getBody()); 
   }
 
