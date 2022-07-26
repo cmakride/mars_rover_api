ed707ec7816954fbc9ac40b1ee98951fd51f940c
ChrisMak <cmakride@gmail.com>
2022-07-26 09:55:00 -0400

    Button Updates depending on current rover that is showing, update to Readme

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # mars_rover_api
+
+This is an app that connects to the NASA API of Mars Rover Pictures
+
+# Technologies Used:
+![Java](https://img.shields.io/badge/java-%23ED8B00.svg?style=for-the-badge&logo=java&logoColor=white)
+![Spring](https://img.shields.io/badge/spring-%236DB33F.svg?style=for-the-badge&logo=spring&logoColor=white)
+![Thymeleaf](https://img.shields.io/badge/Thymeleaf-%23005C0F.svg?style=for-the-badge&logo=Thymeleaf&logoColor=white)
+
+![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)
+![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white)
+
+![MySQL](https://img.shields.io/badge/mysql-%2300f.svg?style=for-the-badge&logo=mysql&logoColor=white)
+
+![Bootstrap](https://img.shields.io/badge/bootstrap-%23563D7C.svg?style=for-the-badge&logo=bootstrap&logoColor=white)
--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -19,13 +19,39 @@
 // Improved Syntax for doing same thing as above
 const marsApiButtons = document.querySelectorAll("button[id*='marsApi']")
 
-marsApiButtons.forEach( button => button.addEventListener('click',function(){
+marsApiButtons.forEach(button => button.addEventListener('click', function () {
   let buttonId = button.id
   let currentId = buttonId.split("marsApi")
   const roverId = currentId[1]
   let apiData = document.getElementById('marsApiRoverData')
   apiData.value = roverId.toLowerCase()
-  console.log(apiData.value)
   // so what should happen is when form is submitted we should get access to that data in the body of the post, which is in homecontroller PostMapping
   document.getElementById('formRoverType').submit()
 }))
+
+//accessing the url to find out what rover currently on so can change the button color
+function getUrlParameter(name) {
+  name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]')
+  var regex = new RegExp('[\\?&]' + name + '=([^&#]*)')
+  var results = regex.exec(location.search)
+  return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '))
+}
+
+let marsRoverType = getUrlParameter("marsApiRoverData")
+marsRoverType = capitalizeFirstLetter(marsRoverType)
+
+//? Change the color of the button
+highLightBtnByRoverType(marsRoverType)
+
+// Functions
+function highLightBtnByRoverType(roverType) {
+  if (roverType == '') {
+    roverType = "Opportunity"
+  }
+  document.getElementById(`marsApi${roverType}`).classList.remove('btn-secondary')
+  document.getElementById(`marsApi${roverType}`).classList.add('btn-primary')
+}
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
